88a079d85b62410f9a3891298802fce387115917
Yong (Leo) Li <4179e1@gmail.com>
2023-01-25 22:19:15 -0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #docker run -it --rm --name dlwpt --gpus '"device=7"' -p 8080:8888 -v `pwd`:/workspace nvcr.io/nvidia/pytorch:22.08-py3 /bin/bash
-docker run -it --rm --name torch --shm-size=10240m --gpus '"device=6,7"' -p 8080:8888 -p 6006:6006 -v `pwd`:/workspace nvcr.io/nvidia/pytorch:22.08-py3  bash -c 'source /usr/local/nvm/nvm.sh; jupyter labextension install @jupyterlab/toc; pip install torchinf
-; jupyter-notebook'
+docker run -it --rm --name torch --shm-size=10240m --gpus '"device=6,7"' -p 8080:8888 -p 6006:6006 -v `pwd`:/workspace nvcr.io/nvidia/pytorch:22.08-py3  bash -c 'source /usr/local/nvm/nvm.sh; jupyter labextension install @jupyterlab/toc; pip install torchinf; jupyter-notebook'
